b0612db8322811323e2efc7605ebfde961182cd8
Sean Jun <seanjun21@gmail.com>
2020-03-20 00:56:54 -0700

    Change title

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -1,7 +1,7 @@
 var CONST = require("./const");
 
 module.exports = {
-  title: `Today Sean Learned`,
+  title: `Today I Learned`,
   description: `Sean's Personal Wiki (Today I Learned)`,
   base: "/TIL/",
   dest: 'build',
